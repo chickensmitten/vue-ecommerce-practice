464803ea72622b0d499b915b7930abcc33489f90
chickensmitten <chia.shengyeong@gmail.com>
2020-05-17 10:55:39 +0800

    More code for styling, @media screen is to set css for smaller screens

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <appHeader></appHeader>
     <appHome></appHome>
+    <appMenu></appMenu>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Home from './components/Home.vue'
+import Header from './components/Header'
+import Home from './components/Home'
+import Menu from './components/Menu'
 
 export default {
   name: 'app',
   components: {
     appHeader: Header,
-    appHome: Home
+    appHome: Home,
+    appMenu: Menu
   }
 }
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="menu_wrapper">
+    <!-- menu -->
+    <div class="menu">
+      <h3>~ Authentic handmade pizza ~</h3>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <strong>
+                ~ Pepperoni ~
+              </strong>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <small>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, ex voluptatum? Porro sit incidunt minima recusandae, vitae similique dolor sequi maxime cum eum facere voluptates velit libero eaque nesciunt est!
+              </small>
+            </td>
+          </tr>   
+          <tr>
+            <td>9"</td>
+            <td>$6.95</td>
+            <td>
+              <button class="btn_green" type="button">+</button>
+            </td>
+          </tr>       
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  h3 {
+    text-align: center;
+  }
+
+  .menu_wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .menu {
+    background: #f1e6da;
+    border-radius: 3px;
+    height: 100vh;
+    margin: 10px;
+    padding: 10px;
+  }
+
+  @media screen and (min-width: 900px) {
+    .menu_wrapper {
+      flex-direction: row;
+      justify-content: space-between;
+    }    
+
+    .menu {
+      width: 65vw;
+    }
+  }
+</style>

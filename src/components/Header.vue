f25db31c8060f8e4234e199e40dbe86d15dd68c7
chickensmitten <chia.shengyeong@gmail.com>
2020-05-17 10:28:59 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <header>
+    <div class="header_logo">
+      <img src="../assets/images/planet.png" alt="Pizza Planet Icon">
+      <h1 href='/'>PIZZA PLANET</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <span>~</span>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <span>~</span>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <span>~</span> 
+          <li>
+            <a href="/about">About</a>
+          </li>        
+        </ul>      
+      </nav>      
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'appHeader'
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 1.3em;
+}
+
+.header_logo {
+  background-image: linear-gradient(
+    45deg,
+    #d85226 25%,
+    #f79e38 25% 50%,
+    #d85226 50% 75%,
+    #f79e38 70%,
+    #f79e38 100%,
+  );
+  background-size: 80px 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10px;
+}
+
+.header_logo img {
+  max-width: 25vw;
+}
+
+ul {
+  padding: 10px 0;
+}
+
+li {
+  display: inline-block;
+}
+
+@media screen and (min-width: 900px) {
+  .header_logo img {
+    max-width: 20vw;
+  }
+}
+</style>

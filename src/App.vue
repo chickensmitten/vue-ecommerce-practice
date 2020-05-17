d01f845690bd2fb3de7c5b7c0e46a03ef4a2fc62
chickensmitten <chia.shengyeong@gmail.com>
2020-05-17 11:39:53 +0800

    Added code for admin page

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,23 @@
   <div id="app">
     <appHeader></appHeader>
     <appHome></appHome>
-    <appMenu></appMenu>
+    <!-- <appMenu></appMenu> -->
+    <appAdmin></appAdmin>
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Home from './components/Home'
-import Menu from './components/Menu'
+import Header from './components/Header';
+import Home from './components/Home';
+// import Menu from './components/Menu';
+import Admin from './components/Admin';
 
 export default {
   name: 'app',
   components: {
     appHeader: Header,
     appHome: Home,
-    appMenu: Menu
+    appAdmin: Admin
   }
 }
 </script>

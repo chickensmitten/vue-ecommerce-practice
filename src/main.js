f0b0359823bd88c17af679ba890baf00d93247cf
chickensmitten <chia.shengyeong@gmail.com>
2020-05-17 14:38:16 +0800

    Adding child routes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home'
 import Menu from './components/Menu'
+import About from './components/About'
+import Delivery from './components/Delivery'
+import OrderingGuide from './components/Ordering'
+import Contact from './components/Contact'
+import Admin from './components/Admin'
 
 Vue.config.productionTip = false
 
@@ -11,6 +16,13 @@ Vue.use(VueRouter);
 const routes = [
   { path: '/', component: Home },
   { path: '/menu', component: Menu },
+  { path: '/contact', component: Contact },
+  { path: '/admin', component: Admin },
+  { path: '/about', component: About, children: [
+    { path: '/history', component: History },
+    { path: '/delivery', component: Delivery },
+    { path: '/ordering-guide', component: OrderingGuide },
+  ]},
   { path: '*', redirect: '/'} // covering for unrecognized URL
 ]
 const router = new VueRouter({
